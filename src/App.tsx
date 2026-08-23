49e2a0c24b1864195a4b3ff7cc2d2863513cd61a
K <guswjd9994@duksung.ac.kr>
2026-08-23 15:43:37 +0900

    [fix] App.tsx의 ReactGA 중복 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import ReactGA from 'react-ga4'
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate, Navigate, useLocation } from 'react-router-dom';
 import ReactGA from 'react-ga4';
